3fb4abdc0b0e7162927af9cda3ab77639bfbf2ee
Hitesh <hitesh.joshi@consensys.net>
2020-04-13 21:20:40 +0800

    market token

--- contracts/TokenExchange.sol
+++ contracts/TokenExchange.sol
@@ -9,20 +9,90 @@ import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/SafeERC20
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/lifecycle/Pausable.sol";
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/StandaloneERC20.sol";
+
+import "./MyToken.sol";
 
 
 contract TokenExchange is Initializable, Ownable, Pausable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
 
-    // Contract state: exchange rate and token
-    uint256 public rate;
-    IERC20 public token;
+    event DisableToken(uint256 indexed tokenId);
+    event EnableToken(uint256 indexed tokenId);
 
-    // Initializer function (replaces constructor) -10, 0xabc
-    function initialize(uint256 _rate, IERC20 _token) public initializer {
-        rate = _rate;
-        token = _token;
+    event TokenAdded(
+        uint256 indexed platformTokenId,
+        address indexed platformToken,
+        address indexed stableCoinAddress
+    );
+
+    mapping(uint256 => address) public tokenIdToStableCoinAddressMap;
+    mapping(uint256 => address) public tokenIdToPlatformCoinAddressMap;
+    mapping(address => uint256) public stableCoinAddressToTokenIdMap;
+    mapping(address => uint256) public platformCoinAddressToTokenIdMap;
+
+    mapping(uint256 => bool) public disabledTokenIdMap;
+
+    uint256[] public platformTokenIds;
+
+    modifier whenNotPaused() {
+        require(!paused(), "ECOSYSTEM_PAUSED");
+        _;
+    }
+
+    modifier whenPaused() {
+        require(paused(), "ECOSYSTEM_NOT_PAUSED");
+        _;
+    }
+
+    modifier checkTokenExists(uint256 tokenId) {
+        require(
+            tokenIdToPlatformCoinAddressMap[tokenId] != address(0x0),
+            "TOKEN_DOES_NOT_EXIST"
+        );
+        _;
+    }
+
+    function initialize() public initializer {
+        Ownable.initialize(msg.sender);
+        Pausable.initialize(msg.sender);
+    }
+
+    /**
+     * @dev Adds a new stable coin to the exchange and mints corresponding platform tokens
+     * @notice Admin function to add a new stable coin market
+     */
+    function addTokenToExchange(
+        address stableCoinAddress,
+        string memory name,
+        string memory symbol,
+        uint8 decimals,
+        uint256 mintAmount
+    ) public onlyOwner whenNotPaused {
+        require(
+            stableCoinAddressToTokenIdMap[stableCoinAddress] == 0,
+            "TOKEN_ALREADY_EXISTS"
+        );
+
+        address[] memory minterArray = new address[](1);
+        minterArray[0] = msg.sender;
+        address[] memory pauserArray = new address[](1);
+        pauserArray[0] = msg.sender;
+
+        StandaloneERC20 platformToken = new StandaloneERC20();
+
+        // Mint platform tokens and assign them to the exchange contract
+        platformToken.initialize(
+            name,
+            symbol,
+            decimals,
+            mintAmount,
+            address(this),
+            minterArray,
+            pauserArray
+        );
+        _addToken(address(platformToken), stableCoinAddress);
     }
 
     // this is some admin on DeepAuto who has authorization of let say 10 USDC
@@ -39,8 +109,68 @@ contract TokenExchange is Initializable, Ownable, Pausable {
             _investmentAmount
         );
 
-        // send some tokens back 10,10 = 100
-        uint256 tokensInternal = _investmentAmount.mul(rate);
-        token.transfer(_investor, tokensInternal);
+        uint256 tokenId = stableCoinAddressToTokenIdMap[_stableCoinAddress];
+        address underlyingTokenAddrs = tokenIdToPlatformCoinAddressMap[tokenId];
+        IERC20 underlyingCoin = IERC20(underlyingTokenAddrs);
+
+        // hardcoded Interest rate of 1
+        uint256 tokensInternal = _investmentAmount.mul(1);
+        underlyingCoin.transfer(_investor, tokensInternal);
+    }
+
+    function enableTokenMarket(uint256 platformTokenId)
+        public
+        checkTokenExists(platformTokenId)
+        whenNotPaused
+        onlyOwner
+    {
+        require(disabledTokenIdMap[platformTokenId], "MARKET_ALREADY_ENABLED");
+        disabledTokenIdMap[platformTokenId] = false;
+        emit EnableToken(platformTokenId);
+    }
+
+    function disableTokenMarket(uint256 platformTokenId)
+        public
+        checkTokenExists(platformTokenId)
+        whenNotPaused
+        onlyOwner
+    {
+        require(
+            !disabledTokenIdMap[platformTokenId],
+            "MARKET_ALREADY_DISABLED"
+        );
+        disabledTokenIdMap[platformTokenId] = true;
+        emit DisableToken(platformTokenId);
+    }
+
+    function _addToken(address platformToken, address stableCoinAddress)
+        private
+    {
+        // Start the IDs at 1. Zero is reserved for the empty case when it doesn't exist.
+        uint256 platformTokenId = platformTokenIds.length + 1;
+
+        // Update the maps
+        tokenIdToPlatformCoinAddressMap[platformTokenId] = platformToken;
+        platformCoinAddressToTokenIdMap[platformToken] = platformTokenId;
+        stableCoinAddressToTokenIdMap[stableCoinAddress] = platformTokenId;
+        tokenIdToStableCoinAddressMap[platformTokenId] = stableCoinAddress;
+
+        disabledTokenIdMap[platformTokenId] = false;
+        platformTokenIds.push(platformTokenId);
+
+        emit TokenAdded(platformTokenId, platformToken, stableCoinAddress);
+    }
+
+    function getPlatformToken(address _stableCoinAddress)
+        public
+        view
+        returns (address)
+    {
+        uint256 tokenId = stableCoinAddressToTokenIdMap[_stableCoinAddress];
+
+
+            address underlyingTokenAddress
+         = tokenIdToPlatformCoinAddressMap[tokenId];
+        return underlyingTokenAddress;
     }
 }
--- test/tokenExchange.js
+++ test/tokenExchange.js
@@ -1,8 +1,6 @@
 const TokenExchange = artifacts.require("./TokenExchange.sol");
 const Token = artifacts.require("./MyToken.sol");
 
-// const should = require("chai").use(require("chai-as-promised")).should();
-
 const BigNumber = web3.utils.BN;
 
 let exchange;
@@ -20,52 +18,79 @@ contract("Token_management", async (accounts) => {
   [accountA, accountB, accountC, accountD] = accounts;
 
   beforeEach(async () => {
-    exchange = await TokenExchange.deployed();
+    exchange = await TokenExchange.new();
+
     usdc = await Token.new();
-    tusdc = await Token.new();
-    await usdc.initialize("usd", "usd", 18, tokenAmount, accountA, [], []);
-    await tusdc.initialize("tusd", "tusd", 18, tokenAmount, accountB, [], []);
-    await exchange.initialize(1, tusdc.address);
+    // tusdc = await Token.new();
+    await usdc.initialize("usd", "usd", 18, tokenAmount, accountD, [], []);
+    // await tusdc.initialize("tusd", "tusd", 18, tokenAmount, accountB, [], []);
+    // await exchange.initialize(1, tusdc.address);
+
+    // new changes
   });
 
   it("Balance", async () => {
-    let usdcBalance = await usdc.balanceOf(accountA);
-    let tUsdcBalance = await tusdc.balanceOf(accountB);
+    let usdcBalance = await usdc.balanceOf(accountD);
+    // let tUsdcBalance = await tusdc.balanceOf(accountB);
 
     assert.equal(usdcBalance.toString(), tokenAmount);
-    assert.equal(tUsdcBalance.toString(), tokenAmount);
+
+    // assert.equal(tUsdcBalance.toString(), tokenAmount);
 
     // transfer  entire tUsdc to exchange
-    tusdc.transfer(exchange.address, tokenAmount, { from: accountB });
-    let tUsdcExchangeBal = await tusdc.balanceOf(exchange.address);
+    // tusdc.transfer(exchange.address, tokenAmount, { from: accountB });
+    // let tUsdcExchangeBal = await tusdc.balanceOf(exchange.address);
 
-    assert.equal(tUsdcExchangeBal.toString(), tokenAmount);
+    // assert.equal(tUsdcExchangeBal.toString(), tokenAmount);
 
     // remaning balance should be zero
-    tUsdcBalance = await tusdc.balanceOf(accountB);
-    assert.equal(tUsdcBalance.toString(), 0);
+    // tUsdcBalance = await tusdc.balanceOf(accountB);
+    // assert.equal(tUsdcBalance.toString(), 0);
 
-    let tUsdcBalanceOfA = await tusdc.balanceOf(accountA);
-    assert.equal(tUsdcBalanceOfA.toString(), 0);
+    // let tUsdcBalanceOfA = await tusdc.balanceOf(accountA);
+    // assert.equal(tUsdcBalanceOfA.toString(), 0);
 
-    // Investor A  allocates some usdc to exchange
-    await usdc.approve(exchange.address, usdcAllocated, { from: accountA });
+    exchange.initialize({ from: accountA });
+    let owner = await exchange.owner();
+    console.log("Onwer ", owner);
+
+    // finally real shit
+    await exchange.addTokenToExchange(
+      usdc.address,
+      "USD Platform",
+      "MUS",
+      18,
+      tokenAmount,
+      { from: accountA }
+    );
+
+    // Investor D  allocates some usdc to exchange
+    await usdc.approve(exchange.address, usdcAllocated, { from: accountD });
 
     // transfer usdc and get tUsdc BACK
-    await exchange.depositStableCoin(usdc.address, accountA, usdcAllocated, {
-      from: accountC,
+    await exchange.depositStableCoin(usdc.address, accountD, usdcAllocated, {
+      from: accountA,
     });
 
     // CHECK FINAL BALANCES
     // accountA has tUSDC now
-    tUsdcBalanceOfA = await tusdc.balanceOf(accountA);
-    assert.equal(tUsdcBalanceOfA.toString(), usdcAllocated);
+    // tUsdcBalanceOfA = await tusdc.balanceOf(accountA);
+    // assert.equal(tUsdcBalanceOfA.toString(), usdcAllocated);
 
     // accountA 's usdc balance is reduced
-    usdcBalance = await usdc.balanceOf(accountA);
+    usdcBalance = await usdc.balanceOf(accountD);
     assert.equal(usdcBalance.toString(), tokenAmount - usdcAllocated);
 
     exchangeUsdcBal = await usdc.balanceOf(exchange.address);
     assert.equal(exchangeUsdcBal.toString(), usdcAllocated);
+
+    let underlyingTokenAddress = await exchange.getPlatformToken(usdc.address);
+
+    let tokenUnderlying = await Token.at(underlyingTokenAddress);
+
+    // check D's Balance
+    underlyingTokenBal = await tokenUnderlying.balanceOf(accountD);
+    console.log(underlyingTokenBal.toString());
+    assert.equal(underlyingTokenBal.toString(), usdcAllocated);
   });
 });
